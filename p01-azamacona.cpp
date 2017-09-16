d96c2f30132d8b3401b691b770356078e657e775
KarinaZamacona <azamacona@mail.csuchico.edu>
2017-09-16 14:14:40 -0700

    p01-azamacona.cpp

---
+++ p01-azamacona.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main ()
+{
+
+    srand ( (unsigned) time( NULL ) ); //random seed
+    int randomNumber, guess;
+    
+    cout<<"Guess the computer's number!"<<endl;
+    cin>>guess;
+    randomNumber = rand()%10+1
+
+if (x==y)
+{
+    cout<< "You, guessed correctly! You win!"<<endl;
+}
+else
+{ 
+    cout<< "Wrong"<<endl;
+}
+
+ return 0;
+ }
