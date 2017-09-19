117ed5291fba19abd381400212f829998c93d799
KarinaZamacona <azamacona@mail.csuchico.edu>
2017-09-19 13:47:43 -0700

    easy, medium, hard guess

--- p01-azamacona.cpp
+++ p01-azamacona.cpp
@@ -6,22 +6,83 @@ using namespace std;
 
 int main ()
 {
+        string x;
 
-        srand ( (unsigned) time( NULL ) ); //random seed
-        int randomNumber, guess;
-        randomNumber = rand()%10+1;
-                do
+        srand ( time( NULL ) ); //random seed
+        int randomNumber,  guess;
+        do
+        {
+                cout<<"Select a difficulty level: Easy, Medium, or Hard"<<endl;
+                cout<<"Select Q to quit game"<<endl;
+                cin>>x;
+                if(x== "Easy")
                 {
-                        cout<<"Guess the computer's number!"<<endl;
+                        cout<<"Guess a number between 1 and 2!"<<endl;
                         cin>>guess;
+                        randomNumber=rand()%2+1;
+
                         if (randomNumber==guess)
                         {
                                 cout<< "You, guessed correctly! You win!"<<endl;
                         }
                         else
                         { 
-                                cout<< "Try again! You were off by: "<< abs(randomNumber-guess)<<endl;
+                                cout<< "You lose, you were off by: "<< abs(randomNumber-guess)<<endl;
                         }
-                }while (randomNumber!=guess);
+                }
+                else if(x=="Medium")
+                {
+                        cout<<"Guess a number between 1 and 10!"<<endl;
+                        randomNumber=rand()%10+1;
+                        for(int i=0; i<2;i++)
+                        {
+                                cin>>guess;
+                                if (randomNumber==guess)
+                                {
+                                        cout<<"You, guessed correctly! You win!"<<endl;
+                                        break;
+                                }
+                                else if (randomNumber<guess)
+                                {
+                                        cout<<"Try one more time, guess lower!"<<endl;
+                                }
+                                else
+                                {
+                                        cout<<"Try one more time, guess higher!"<<endl;
+                                }
+                        }
+                        if (randomNumber!=guess)
+                        {
+                                cout<<"You were off by: "<<abs(randomNumber-guess)<< ". Sorry you lose!"<<endl;
+                        }
+                }
+                else if(x=="Hard")
+                {
+                        cout<<"Guess a number between 1 and 100!"<<endl;
+                        randomNumber=rand()%100+1;
+                        for(int i=0; i<3;i++)
+                        {
+                                cin>>guess;
+                                if (randomNumber==guess)
+                                {
+                                        cout<<"You, guessed correctly! You win!"<<endl;
+                                        break;
+                                }
+                                else if (randomNumber<guess)
+                                {
+                                        cout<<"Try one more time, guess lower!"<<endl;
+                                }
+                                else
+                                {
+                                        cout<<"Try one more time, guess higher!"<<endl;
+                                }
+                        }
+                        if (randomNumber!=guess)
+                        {
+                                cout<<"You were off by: "<<abs(randomNumber-guess)<< ". Sorry you lose!"<<endl;
+                        }
+                }
+
+        }while (x!="Q");
         return 0;
 }
